b9a602dd93886113a89e234eb1163bbc2fda0d91
Jeff <jeffrey.kl.hui@gmail.com>
2019-02-27 18:03:07 -0800

    mvp stable v.1.0

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, flash
 from flask_cors import CORS
 from pymongo import MongoClient
 import os
@@ -23,6 +23,7 @@ db = client.get_database("coatcheck")
 jacket = db.jacket
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 CORS(app)
 
 '''
@@ -57,37 +58,20 @@ Routing
 def index():
 	return render_template("index.html")
 
-@app.route('/corp')
-def corpsjacket():
-	return render_template("corps.html")
-
-@app.route('/register', methods=['POST'])
-def register():
-	if request.form['Password'] == entry:
-		addUser(request.form['Phone'],request.form['Size'])
-		return "Hold Tight, we'll let you know when it comes in stock!"
-	return "Check the form and try again"
-
-# @app.route('/addMe',methods=['POST'])
-# def sms():
-# 	number = request.form['From']
-# 	message_body = request.form['Body']
-# 	resp = MessagingResponse()
-# 	if entry in message_body.lower():
-# 		size = message_body.split(' ')[1].lower() in ['s','m','l','xl','xxl']
-# 		if size and addUser(number,message_body.split(' ')[1].lower()):
-# 			resp.message("Alright, I gotchu. You're in")
-# 		else: 
-# 			resp.message("Looks like you're already on the list")
-# 	elif "serverstatus" in message_body.lower():
-#             resp.message("Server Health: Good")
-
-# 	else:
-# 		resp.message("Invalid")
-# 	return str(resp)
-
-
-
+@app.route('/corp', methods=['GET','POST'])
+def corp():
+	record = jacket.find_one({'name':"history"})
+	output = record["stock"]
+	if request.method == "POST":
+		if request.form['password'] == entry and len(request.form['name']) == 10 and request.form['name'].isdigit():
+			if getUser("+1"+ request.form["name"]) == None:
+				addUser("+1" + request.form['name'],request.form['size'])
+				flash("You're on the list. We'll let you know!", "success")
+			else:
+				flash("Wow! We like you too, but it looks like you've already signed up. We'll keep you posted!", "success")
+		else:
+			flash("Yikes! Check your contact information or secret password", "danger")
+	return render_template("corps.html", small=output["s"],med=output["m"],large=output['l'],xl=output['xl'],xxl=output['xxl'],threexl=output['3xl'])
 
 
 
@@ -105,9 +89,23 @@ def sms():
 	elif "serverstatus" in message_body.lower():
             resp.message("Server Health: Good")
 
-	else:
+        elif "reset" in message_body.lower():
+            found = getUser(number)
+            if found != None:
+                found['notified'] = False
+                jacket.update({"name":number},found)
+                resp.message("Okay! You're back on the list")
+            else:
+                resp.message("Can't find you")
+        else:
 		resp.message("Invalid")
 	return str(resp)
+
+@app.route('/justfortest')
+def test():
+    test_str = '{"Black":[{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":false,"size":"S","sku":"100036703","priority":0},{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":false,"size":"M","sku":"100036702","priority":0},{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":false,"size":"L","sku":"100036701","priority":0},{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":false,"size":"XL","sku":"100036704","priority":0},{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":false,"size":"XXL","sku":"100036705","priority":0},{"color":"Black","name":"Mens Corp Jacket","price":"$120","purchasable":true,"size":"3XL","sku":"100036706","priority":0}]}'
+    output = json.loads(test_str)
+    return jsonify(output)
 
 @app.errorhandler(500)
 def server_error(e):
--- check.py
+++ check.py
@@ -6,7 +6,7 @@ import requests
 import pymongo
 from pymongo import MongoClient
 from twilio.rest import Client
-
+import datetime as dt
 
 acc_sid, auth_t, twil_num = TwilioConfig()
 user, pwrd, location = DBConfig()
@@ -22,6 +22,12 @@ jacket = db.jacket
 def sendMessage(twilioclient,msg_body,msg_from,msg_to):
     message = twilioclient.messages.create(body=msg_body,from_=msg_from,to=msg_to)
 
+def updateLastStock(size):
+	a = dt.datetime.now()
+	record = jacket.find_one({'name':'history'})
+	record['stock'][size] = str(a).split('.')[0]
+	jacket.update({'name':'history'},record)
+
 def notify(size):
 	luckyOnes = jacket.find({'size': size})
 	for person in luckyOnes:
@@ -29,7 +35,8 @@ def notify(size):
 			message = "Quick! Your jacket is in stock NOW in size: " + person['size'].upper()
 			sendMessage(tw_client,message,twil_num,person['name'])
 			jacket.update(	{
-							'name': person['name']
+							'name': person['name'],
+							'size': person['size']
 							}, 
 							{
 								'name': person['name'],
@@ -41,10 +48,15 @@ def runner():
 	res = requests.get(payload).json()
 	msg = ""
 	for sizes in res["Black"]:
+		print(sizes['size'])
 		msg += "Size: " + sizes["size"] + "\n" + "Stock: " + ("yes" if sizes["purchasable"] else "no") + "\n"
 		if sizes["purchasable"]:
-			notify(sizes["purchasable"])
+			inStock = sizes["size"].lower()
+			notify(inStock)
+			updateLastStock(inStock)
 	print(msg)
 
 if __name__=="__main__":
-	runner()	
+	runner()
+
+
