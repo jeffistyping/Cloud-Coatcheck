6b2fa0d45fb1b9179d3021db845740803d21ee77
Jeff Hui <jeffrey.hui@shopify.com>
2019-10-03 19:01:02 -0400

    formatting changes

--- app.py
+++ app.py
@@ -41,63 +41,63 @@ CORS(app)
 Database Access Utilities
 '''
 def getUser(name):
-	record = jacket.find_one({'name': name})
-	return record
+  record = jacket.find_one({'name': name})
+  return record
 
 def addUser(username, size,item):
-	'''
-	Params: Username, size, item
-	Function: Adds new user to database if not exist
-	Returns: Boolean if user is added
-	'''
-	if getUser(username) is None:
-	    record = {
-	    	'name': username,
-	    	'size': size,
-	    	'notified': False,
-			'item': item
-	    }
-	    jacket.insert_one(record)
-	    return True
-	return False
+  '''
+  Params: Username, size, item
+  Function: Adds new user to database if not exist
+  Returns: Boolean if user is added
+  '''
+  if getUser(username) is None:
+      record = {
+        'name': username,
+        'size': size,
+        'notified': False,
+      'item': item
+      }
+      jacket.insert_one(record)
+      return True
+  return False
 
 def humanTime(sizes,lastStockDict):
-	'''
-	Params: Last Stock Time, Sizes
-	Function: Output relative time in natural language
-	Returns: Dictionary of relative time mapped to product
-	'''
-	now = dt.now()
-	humanOutput = {}
-	for size in sizes:
-		if lastStockDict[size] != "No Data":
-			delta = now - dt.strptime(lastStockDict[size],"%Y-%m-%d %H:%M:%S.%f")
-			totalTime = delta.total_seconds()
-			if totalTime/ 31622400 > 1:
-				humanOutput[size] = str(int(math.floor(totalTime/31622400))) + " year(s) ago"
-			elif totalTime/31622400 == 1:
-				humanOutput[size] = "1 year ago"
-			elif totalTime/604800 > 1:
-				humanOutput[size] = str(int(math.floor(totalTime/604800))) + " weeks ago"
-			elif totalTime/604800 == 1:
-				humanOutput[size] = "1 week ago"
-			elif totalTime/86400 > 1:
-				humanOutput[size] = str(int(math.floor(totalTime/86400))) + " days ago"
-			elif totalTime/86400 == 1:
-				humanOutput[size] = "1 day ago"
-			elif totalTime/3600 > 1:
-				humanOutput[size] = str(int(math.floor(totalTime/3600))) + " hours ago"
-			elif totalTime/3600 == 1:
-				humanOutput[size] = "1 hour ago"
-			elif totalTime/60 > 1:
-				humanOutput[size] = str(int(math.floor(totalTime/3600))) + " minutes ago"
-			elif totalTime/60 == 1:
-				humanOutput[size] = "1 minute ago"
-			else:
-				humanOutput[size] = "In Stock Now"
-		else:
-			humanOutput[size] = "No Data"	
-	return humanOutput
+  '''
+  Params: Last Stock Time, Sizes
+  Function: Output relative time in natural language
+  Returns: Dictionary of relative time mapped to product
+  '''
+  now = dt.now()
+  humanOutput = {}
+  for size in sizes:
+    if lastStockDict[size] != "No Data":
+      delta = now - dt.strptime(lastStockDict[size],"%Y-%m-%d %H:%M:%S.%f")
+      totalTime = delta.total_seconds()
+      if totalTime/ 31622400 > 1:
+        humanOutput[size] = str(int(math.floor(totalTime/31622400))) + " year(s) ago"
+      elif totalTime/31622400 == 1:
+        humanOutput[size] = "1 year ago"
+      elif totalTime/604800 > 1:
+        humanOutput[size] = str(int(math.floor(totalTime/604800))) + " weeks ago"
+      elif totalTime/604800 == 1:
+        humanOutput[size] = "1 week ago"
+      elif totalTime/86400 > 1:
+        humanOutput[size] = str(int(math.floor(totalTime/86400))) + " days ago"
+      elif totalTime/86400 == 1:
+        humanOutput[size] = "1 day ago"
+      elif totalTime/3600 > 1:
+        humanOutput[size] = str(int(math.floor(totalTime/3600))) + " hours ago"
+      elif totalTime/3600 == 1:
+        humanOutput[size] = "1 hour ago"
+      elif totalTime/60 > 1:
+        humanOutput[size] = str(int(math.floor(totalTime/3600))) + " minutes ago"
+      elif totalTime/60 == 1:
+        humanOutput[size] = "1 minute ago"
+      else:
+        humanOutput[size] = "In Stock Now"
+    else:
+      humanOutput[size] = "No Data"	
+  return humanOutput
 
 
 '''
@@ -106,77 +106,77 @@ Routing
 
 @app.route('/')
 def index():
-	'''
-	get: renders home page
-	'''
-	return render_template("index.html")
+  '''
+  get: renders home page
+  '''
+  return render_template("index.html")
 
 @app.route('/mens_corp', methods=['GET','POST'])
 def mens_corp():
-	'''
-	get: renders live tracking page for men's corps jacket
-	post: submit name for men's corps jacket notification list 
-	'''
-	record = jacket.find_one({'name':"stock"})
-	output = record["mens_corps_stock"]
-	sizes = ['s','m','l','xl','xxl','3xl']
-	options = humanTime(sizes,output)
-	if request.method == "POST":
-		if request.form['password'] == entry:
-			if getUser("+1"+ request.form["name"]) == None:
-				addUser("+1" + request.form['name'],request.form['size'],"mens_corps")
-				flash("You're on the list. We'll let you know!", "success")
-			else:
-				flash("Wow! We like you too, but it looks like you've already signed up. We'll keep you posted!", "success")
-		else:
-			flash("Yikes! Check your contact information or secret password", "danger")
-	return render_template("corps.html", options=options, postname="mens_corp", itemname="Men's Corps Jacket", sizes=sizes)
+  '''
+  get: renders live tracking page for men's corps jacket
+  post: submit name for men's corps jacket notification list 
+  '''
+  record = jacket.find_one({'name':"stock"})
+  output = record["mens_corps_stock"]
+  sizes = ['s','m','l','xl','xxl','3xl']
+  options = humanTime(sizes,output)
+  if request.method == "POST":
+    if request.form['password'] == entry:
+      if getUser("+1"+ request.form["name"]) == None:
+        addUser("+1" + request.form['name'],request.form['size'],"mens_corps")
+        flash("You're on the list. We'll let you know!", "success")
+      else:
+        flash("Wow! We like you too, but it looks like you've already signed up. We'll keep you posted!", "success")
+    else:
+      flash("Yikes! Check your contact information or secret password", "danger")
+  return render_template("corps.html", options=options, postname="mens_corp", itemname="Men's Corps Jacket", sizes=sizes)
 
 @app.route('/womens_corp', methods=['GET','POST'])
 def womens_corp():
-	'''
-	get: renders live tracking page for women's corps jacket
-	post: submit name for women's corps jacket notification list 
-	'''
-	record = jacket.find_one({'name':"stock"})
-	output = record["womens_corps_stock"]
-	sizes = ['xs','s','m','l','xl','xxl']
-	options = humanTime(sizes,output)
-	if request.method == "POST":
-		if request.form['password'] == entry:
-			if getUser("+1"+ request.form["name"]) == None:
-				addUser("+1" + request.form['name'],request.form['size'],"womens_corps")
-				flash("You're on the list. We'll let you know!", "success")
-			else:
-				flash("Wow! We like you too, but it looks like you've already signed up. We'll keep you posted!", "success")
-		else:
-			flash("Yikes! Check your contact information or secret password", "danger")
-	return render_template("corps.html", options=options, postname="womens_corp", itemname="Women's Corps Jacket", sizes=sizes)
+  '''
+  get: renders live tracking page for women's corps jacket
+  post: submit name for women's corps jacket notification list 
+  '''
+  record = jacket.find_one({'name':"stock"})
+  output = record["womens_corps_stock"]
+  sizes = ['xs','s','m','l','xl','xxl']
+  options = humanTime(sizes,output)
+  if request.method == "POST":
+    if request.form['password'] == entry:
+      if getUser("+1"+ request.form["name"]) == None:
+        addUser("+1" + request.form['name'],request.form['size'],"womens_corps")
+        flash("You're on the list. We'll let you know!", "success")
+      else:
+        flash("Wow! We like you too, but it looks like you've already signed up. We'll keep you posted!", "success")
+    else:
+      flash("Yikes! Check your contact information or secret password", "danger")
+  return render_template("corps.html", options=options, postname="womens_corp", itemname="Women's Corps Jacket", sizes=sizes)
 
 
 @app.route('/sms',methods=['POST'])
 def sms():
-	'''
-	post: Adds users to the appropriate notification lists from mobile text through Twilio API
-	'''
-	number = request.form['From']
-	message_body = request.form['Body']
-	resp = MessagingResponse()
+  '''
+  post: Adds users to the appropriate notification lists from mobile text through Twilio API
+  '''
+  number = request.form['From']
+  message_body = request.form['Body']
+  resp = MessagingResponse()
 
-	if "serverstatus" in message_body.lower():
-		resp.message("Server Health: Good")
-	
-	elif "reset" in message_body.lower():
-		found = getUser(number)
-		if found != None:
-			found['notified'] = False
-			jacket.update({"name":number},found)
-			resp.message("Okay! You're back on the list")
-		else:
-			resp.message("Can't find you")
-	else:
-		resp.message("Invalid")
-	return str(resp)
+  if "serverstatus" in message_body.lower():
+    resp.message("Server Health: Good")
+  
+  elif "reset" in message_body.lower():
+    found = getUser(number)
+    if found != None:
+      found['notified'] = False
+      jacket.update({"name":number},found)
+      resp.message("Okay! You're back on the list")
+    else:
+      resp.message("Can't find you")
+  else:
+    resp.message("Invalid")
+  return str(resp)
 
 @app.errorhandler(500)
 def server_error(e):
--- check.py
+++ check.py
@@ -24,47 +24,47 @@ def sendMessage(twilioclient,msg_body,msg_from,msg_to):
     message = twilioclient.messages.create(body=msg_body,from_=msg_from,to=msg_to)
 
 def updateLastStock(size,item):
-	'''
-	Params: Size, Item
-	Function: Updates the stock of a specified item and size in the database
-	Returns: None
-	'''
-	a = dt.datetime.now()
-	record = jacket.find_one({'name':'stock'})
-	record[item + '_stock'][size] = str(a)
-	jacket.update({'name':'stock'},record)
+  '''
+  Params: Size, Item
+  Function: Updates the stock of a specified item and size in the database
+  Returns: None
+  '''
+  a = dt.datetime.now()
+  record = jacket.find_one({'name':'stock'})
+  record[item + '_stock'][size] = str(a)
+  jacket.update({'name':'stock'},record)
 
 def notify(size, item):
-	luckyOnes = jacket.find({'size': size,'item': item })
-	for person in luckyOnes:
-		if not person['notified']:
-			message = f"Quick! Your jacket is in stock NOW in size: {person['size'].upper()} \
-			 \nIf you wish you to be placed back onto the notification list, please reply 'reset'"
-			sendMessage(tw_client,message,twil_num,person['name'])
-			person['notified'] = True
-			jacket.update(	{
-							'name': person['name'],
-							'size': person['size'],
-							'item': item},
-							person)
+  luckyOnes = jacket.find({'size': size,'item': item })
+  for person in luckyOnes:
+    if not person['notified']:
+      message = f"Quick! Your jacket is in stock NOW in size: {person['size'].upper()} \
+       \nIf you wish you to be placed back onto the notification list, please reply 'reset'"
+      sendMessage(tw_client,message,twil_num,person['name'])
+      person['notified'] = True
+      jacket.update(	{
+              'name': person['name'],
+              'size': person['size'],
+              'item': item},
+              person)
 
 def runner(payload, item):
-	'''
-	Params: payload API, item of interest
-	Function: Interfaces with shop API
-	Returns: None
-	'''
-	res = requests.get(payload).json()
-	msg = ""
-	print("Item: " + item)
-	for sizes in res["Black"]:
-		msg += "Size: " + sizes["size"] + "\n" + "Stock: " + ("yes" if sizes["purchasable"] else "no") + "\n"
-		if sizes["purchasable"]:
-			inStock = sizes["size"].lower()
-			notify(inStock,item)
-			updateLastStock(inStock,item)
+  '''
+  Params: payload API, item of interest
+  Function: Interfaces with shop API
+  Returns: None
+  '''
+  res = requests.get(payload).json()
+  msg = ""
+  print("Item: " + item)
+  for sizes in res["Black"]:
+    msg += "Size: " + sizes["size"] + "\n" + "Stock: " + ("yes" if sizes["purchasable"] else "no") + "\n"
+    if sizes["purchasable"]:
+      inStock = sizes["size"].lower()
+      notify(inStock,item)
+      updateLastStock(inStock,item)
 
 if __name__=="__main__":
-	runner(m_corps,'mens_corps')
-	runner(w_corps,'womens_corps')
+  runner(m_corps,'mens_corps')
+  runner(w_corps,'womens_corps')
 
